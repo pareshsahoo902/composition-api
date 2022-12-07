c1a95e48e9ff8aa0150c86e2fdbcb75db64880c5
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-12-07 15:00:08 +0530

    added Place to visit app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <course-goals></course-goals>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CourseGoals from "./components/CourseGoals.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CourseGoals,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+  background-color: #0d5e45;
+}
+.container {
+  box-shadow: 0 2px 8px rgba(12, 12, 12, 0.372);
+  max-width: 25rem;
+  margin: 3rem auto;
+  padding: 1rem;
+  border-radius: 12px;
+}
+button {
+  background-color: #155311;
+  border: 1px solid #395745;
+  color: white;
+  border-radius: 12px;
+  font: inherit;
+  cursor: pointer;
+  padding: 0.5rem 1.5rem;
+  margin-right: 1rem;
+}
+button:hover,
+button:active {
+  background-color: #089067;
+  border-color: #033538;
 }
 </style>
---
+++ src/components/GoalsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="container">
+      <h2>Places to Visit</h2>
+      <ul>
+        <li v-for="goal in goals" :key="goal.id">{{ goal.text }}</li>
+      </ul>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    props: ["goals"],
+  };
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    margin: 1rem 0;
+    border: 1px solid #ccc;
+  }
+  </style>
