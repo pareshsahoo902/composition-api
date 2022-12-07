2f9e1e96f66017d8acbf5a1cdae9041e1270d731
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-12-07 15:27:20 +0530

    Added Compositionn API

--- src/components/AddGoal.vue
+++ src/components/AddGoal.vue
@@ -1,59 +1,85 @@
 <template>
-    <section class="container">
-      <form @submit.prevent="addGoal">
-        <div>
-          <label for="goaltext">Travel to</label>
-          <input type="text" id="goaltext" v-model="enteredText" />
-        </div>
-        <p v-if="invalidInput">Please enter a valid goal text (non-empty).</p>
-        <button>Add Destinaiton</button>
-      </form>
-    </section>
-  </template>
+  <section class="container">
+    <form @submit.prevent="addGoal">
+      <div>
+        <label for="goaltext">Travel to</label>
+        <input type="text" id="goaltext" v-model="enteredText" />
+      </div>
+      <p v-if="invalidInput">Please enter a valid Place text (non-empty).</p>
+      <button>Add Destinaiton</button>
+    </form>
+  </section>
+</template>
   
   <script>
-  export default {
-    emits: ["add-goal"],
-    data() {
-      return {
-        enteredText: "",
-        invalidInput: false,
-      };
-    },
-    methods: {
-      addGoal() {
-        this.invalidInput = false;
-        if (this.enteredText === "") {
-          this.invalidInput = true;
-          return;
-        }
-        this.$emit("add-goal", this.enteredText);
-        this.enteredText = '';
-      },
-    },
-    watch: {
-      invalidInput(val) {
-        if (val) {
-          console.log("Analytics: Invalid Input");
-        }
-      },
-    },
-  };
-  </script>
+import { ref, watch } from "vue";
+export default {
+  emits: ["add-goal"],
+  setup(_, context) {
+    const enteredText = ref("");
+    const invalidInput = ref(false);
+
+    function addGoal() {
+      invalidInput.value = false;
+      if (enteredText.value === "") {
+        invalidInput.value = true;
+        return;
+      }
+      context.emit("add-goal", enteredText.value);
+      enteredText.value = "";
+    }
+
+    watch(invalidInput, function (val) {
+      if (val) {
+        console.log("Analytics: Invalid Input");
+      }
+    });
+    return {
+      enteredText,
+      invalidInput,
+      addGoal: addGoal,
+    };
+  },
+  // data() {
+  //   return {
+  //     enteredText: "",
+  //     invalidInput: false,
+  //   };
+  // },
+  // methods: {
+  //   addGoal() {
+  //     this.invalidInput = false;
+  //     if (this.enteredText === "") {
+  //       this.invalidInput = true;
+  //       return;
+  //     }
+  //     this.$emit("add-goal", this.enteredText);
+  //     this.enteredText = '';
+  //   },
+  // },
+  // watch: {
+  //   invalidInput(val) {
+  //     if (val) {
+  //       console.log("Analytics: Invalid Input");
+  //     }
+  //   },
+  // },
+};
+</script>
   
   <style scoped>
-  form {
-    text-align: center;
-  }
-  label {
-    display: block;
-    font-weight: bold;
-    margin-bottom: 0.5rem;
-  }
-  input {
-    font: inherit;
-    display: block;
-    width: 100%;
-    margin-bottom: 0.5rem;
-  }
-  </style>
+form {
+  text-align: center;
+}
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+input {
+  font: inherit;
+  display: block;
+  width: 100%;
+  margin-bottom: 0.5rem;
+}
+</style>
--- src/components/CourseGoals.vue
+++ src/components/CourseGoals.vue
@@ -1,38 +1,43 @@
 <template>
-    <div>
-      <goals-list :goals="filteredGoals"></goals-list>
-      <add-goal @add-goal="addGoal"></add-goal>
-    </div>
-  </template>
+  <div>
+    <goals-list :goals="filteredGoals"></goals-list>
+    <add-goal @add-goal="addGoal"></add-goal>
+  </div>
+</template>
   
   <script>
-  import GoalsList from "./GoalsList.vue";
-  import AddGoal from "./AddGoal.vue";
-  export default {
-    components: {
-      GoalsList,
-      AddGoal,
-    },
-    data() {
-      return {
-        goals: [],
+import GoalsList from "./GoalsList.vue";
+import AddGoal from "./AddGoal.vue";
+import { ref, computed } from "vue";
+export default {
+  components: {
+    GoalsList,
+    AddGoal,
+  },
+  setup() {
+    const goals = ref([]);
+    //addding a computed property
+    const filteredGoals = computed(function () {
+      return goals.value.filter(
+        (goal) => !goal.text.includes("Delhi") && !goal.text.includes("Mumbai")
+      );
+    });
+    //adding a method in setup
+    function addGoal(text) {
+      const newGoal = {
+        id: new Date().toISOString(),
+        text: text,
       };
-    },
-    computed: {
-      filteredGoals() {
-        return this.goals.filter(
-          (goal) => !goal.text.includes("Delhi") && !goal.text.includes("Mumbai")
-        );
-      },
-    },
-    methods: {
-      addGoal(text) {
-        const newGoal = {
-          id: new Date().toISOString(),
-          text: text,
-        };
-        this.goals.push(newGoal);
-      },
-    },
-  };
-  </script>
+      goals.value.push(newGoal);
+    }
+
+    return {
+      goals,
+      filteredGoals: filteredGoals,
+      addGoal: addGoal,
+    };
+    //You can use Options and composition api side by side . as other component is 
+    //using optionsAPi at this time
+  },
+};
+</script>
